0cc3cbcb6c4ba9f5c778ce48e7e3cc8288fd5417
Yury Bondarenko <bndyrv@gmail.com>
2018-10-19 17:46:21 +0200

    Masking & volumes - fix incomplete commit

---
+++ try_masks.py
@@ -0,0 +1,115 @@
+import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+
+def ckernel(size):
+    if not size%2: size = size-1
+    index = int(size/2)
+
+    # mask = np.zeros([size,size])
+    y,x = np.ogrid[-index:size-index, -index:size-index]
+    r = int(size/2)
+    mask = x*x + y*y <= r*r
+    array = np.zeros([size,size], dtype = np.uint8)
+    array[mask] = 255
+    return array
+
+kernel = ckernel(7)
+
+def to_mask(image, kernel):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    ret, image = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+    return np.abs(np.subtract(np.array(image, dtype = np.uint8), 255))
+
+def gray_to_mask(image, kernel):
+    ret, image = cv2.threshold(image, 200, 255, cv2.THRESH_BINARY)
+    image = cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+    return np.abs(np.subtract(np.array(image, dtype=np.uint8), 255))
+
+full = cv2.imread(os.path.join(os.getcwd(), "overlay.png"))
+
+PM = cv2.imread(os.path.join(os.getcwd(), "PM.png"))
+WLC = cv2.imread(os.path.join(os.getcwd(), "WLC.png"))
+SLC = cv2.imread(os.path.join(os.getcwd(), "SLC.png"))
+
+PM_mask = to_mask(PM, kernel)
+WLC_mask = to_mask(WLC, kernel)
+SLC_mask = to_mask(SLC, kernel)
+
+masked = cv2.bitwise_and(full, full, mask = WLC_mask)
+
+# plt.imshow(masked)
+# plt.show()
+
+# area = np.sum(masked > 0)
+
+# Get frame
+cap = cv2.VideoCapture(os.path.join(os.getcwd(), "video.mp4"))
+
+frameN = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+
+cap.set(cv2.CAP_PROP_POS_FRAMES, int(frameN/4))
+ret, frame = cap.read()
+
+# Copied from try_tk_canvas_rectangel.py
+transform = np.array([
+    [-1.24753272e+00,  7.74865041e-03,  1.73556209e+03],
+     [ 3.33306868e-03, -1.24656769e+00,  1.26060824e+03],
+     [6.77439845e-06, 1.01403621e-05, 1.00000000e+00]
+])
+
+Y,X,C = full.shape
+image = cv2.warpPerspective(frame, transform, (X,Y))
+
+frame_PM = cv2.bitwise_and(image, image, mask = PM_mask)
+frame_WLC = cv2.bitwise_and(image, image, mask = WLC_mask)
+frame_SLC = cv2.bitwise_and(image, image, mask = SLC_mask)
+
+from_blue = (90, 50, 50)
+to_blue = (110, 255, 255)
+
+from_red = (140, 5, 10)
+to_red = (180, 255, 255)
+
+hsv = cv2.cvtColor(frame_PM, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hsv, from_blue, to_blue)
+
+frame_PM2 = cv2.bitwise_and(hsv, hsv, mask = mask)
+
+bgr = cv2.cvtColor(frame_PM2, cv2.COLOR_HSV2BGR)
+gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+
+area = np.sum(gray > 0)
+print(area)
+
+cv2.imshow('PM', gray)
+
+hsv = cv2.cvtColor(frame_WLC, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hsv, from_blue, to_blue)
+
+frame_PM2 = cv2.bitwise_and(hsv, hsv, mask = mask)
+
+bgr = cv2.cvtColor(frame_PM2, cv2.COLOR_HSV2BGR)
+gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+
+area = np.sum(gray > 0)
+print(area)
+
+cv2.imshow('WLC', gray)
+
+hsv = cv2.cvtColor(frame_SLC, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hsv, from_red, to_red)
+
+frame_PM2 = cv2.bitwise_and(hsv, hsv, mask = mask)
+
+bgr = cv2.cvtColor(frame_PM2, cv2.COLOR_HSV2BGR)
+gray = cv2.cvtColor(bgr, cv2.COLOR_BGR2GRAY)
+
+area = np.sum(gray > 0)
+print(area)
+
+cv2.imshow('SLC', gray)
+
+cv2.waitKey()
