46b366d2c5f8e4b6e7d55d748e974c64d94241f8
ybnd <ybnd@tuta.io>
2020-03-04 08:13:54 +0100

    Application root directory

---
+++ isimple/core/__init__.py
@@ -0,0 +1,17 @@
+import os
+import pathlib
+
+
+_user_dir = str(pathlib.Path.home())  # todo: where is this on Windows?
+_subdirs = ['.local', 'share', 'isimple']
+
+ROOT = os.path.join(
+    _user_dir, '/'.join(_subdirs)
+)
+
+if not os.path.isdir(ROOT):
+    _path = _user_dir
+    for _subdir in _subdirs:
+        _path = os.path.join(_path, _subdir)
+        if not os.path.isdir(_path):
+            os.mkdir(_path)
