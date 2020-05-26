42f223a51a0d56233a787a31a388e0b1903d58df
ybnd <ybnd@tuta.io>
2020-05-26 11:50:05 +0200

    Split chart stack / result page implementation to recycle for ResultCard

---
+++ ui/components/results/ResultChartStack.vue
@@ -0,0 +1,132 @@
+<template>
+  <div
+    class="result-container"
+    :class="container_class"
+    ref="container"
+    :key="charts_key"
+  >
+    <template v-for="(feature_data, feature, index) in result">
+      <div
+        class="result"
+        :class="result_class"
+        :key="`result-${name}-${feature}`"
+      >
+        <ResultChart
+          :chart-data="{
+            datasets: feature_data,
+            unit: $store.state.options.feature.units[feature]
+          }"
+          :options="{
+            ...options,
+            legend: {
+              display: is_last(index),
+              position: 'bottom'
+            },
+            scales: {
+              xAxes: [
+                {
+                  scaleLabel: {
+                    display: is_last(index),
+                    labelString: 'Time (s)',
+                    fontStyle: 'bold',
+                    fontSize: 14
+                  }
+                }
+              ],
+              yAxes: [
+                {
+                  scaleLabel: {
+                    display: true,
+                    labelString: `${$store.state.options.feature.labels[feature]} (${$store.state.options.feature.units[feature]})`,
+                    fontStyle: 'bold',
+                    fontSize: 14
+                  }
+                }
+              ]
+            }
+          }"
+        />
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+import ResultChart from "../../components/results/ResultChart";
+
+export default {
+  name: "ResultChartStack",
+  props: {
+    container_class: {
+      type: String,
+      default: ""
+    },
+    result_class: {
+      type: String,
+      default: ""
+    },
+    canvas_class: {
+      type: String,
+      default: ""
+    }
+  },
+  components: { ResultChart },
+  beforeMount() {
+    this.updateChartsKey();
+    window.onresize = this.updateChartsKey;
+  },
+  methods: {
+    is_last(index) {
+      return index === Object.keys(this.result).length - 1;
+    },
+    updateChartsKey() {
+      this.charts_key = Date.now();
+    }
+  },
+  computed: {
+    id() {
+      return this.$route.query.id;
+    },
+    name() {
+      return this.$store.getters["analyzers/getName"](this.id);
+    },
+    result() {
+      let result = this.$store.getters["analyzers/getResult"](this.id);
+
+      console.log("store state =");
+      console.log(this.$store.state);
+
+      console.log("result = ");
+      console.log(result);
+      return result;
+    }
+  },
+  data() {
+    return {
+      options: {
+        responsive: true,
+        maintainAspectRatio: false
+      },
+      charts_key: ""
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/scss/_bootstrap-variables";
+@import "../../assets/scss/_core-variables";
+@import "node_modules/bootstrap/scss/functions";
+
+.result-container {
+  display: flex;
+  flex-direction: column;
+  overflow: hidden;
+  padding-top: 8px;
+}
+.result {
+  position: relative;
+  flex-shrink: 1;
+  display: flex;
+}
+</style>
--- ui/pages/analysis/result.vue
+++ ui/pages/analysis/result.vue
@@ -1,94 +1,18 @@
 <template>
   <div class="fixed-page">
-    <div class="result-container" ref="container" :key="charts_key">
-      <template v-for="(feature_data, feature, index) in result" >
-        <div class="result" :key="`result-${name}-${feature}`">
-          <ResultChart
-            :chart-data="{
-              datasets: feature_data,
-              unit: $store.state.options.feature.units[feature]
-            }"
-            :options="{
-              ...options,
-              legend: {
-                display: is_last(index),
-                position: 'bottom'
-              },
-              scales: {
-                xAxes: [
-                  {
-                    scaleLabel: {
-                      display: is_last(index),
-                      labelString: 'Time (s)',
-                      fontStyle: 'bold',
-                      fontSize: 14
-                    }
-                  }
-                ],
-                yAxes: [
-                  {
-                    scaleLabel: {
-                      display: true,
-                      labelString: `${$store.state.options.feature.labels[feature]} (${$store.state.options.feature.units[feature]})`,
-                      fontStyle: 'bold',
-                      fontSize: 14
-                    }
-                  }
-                ]
-              }
-            }"
-          />
-        </div>
-      </template>
-    </div>
+    <ResultChartStack
+      container_class="fullpage-result-container"
+      result_class="fullpage-result"
+    />
   </div>
 </template>
 
 <script>
-import ResultChart from "../../components/results/ResultChart";
+import ResultChartStack from "../../components/results/ResultChartStack";
 
 export default {
   name: "result",
-  components: { ResultChart },
-  beforeMount() {
-    this.updateChartsKey();
-    window.onresize = this.updateChartsKey;
-  },
-  methods: {
-    is_last(index) {
-      return index === Object.keys(this.result).length - 1;
-    },
-    updateChartsKey() {
-      this.charts_key = Date.now()
-    }
-  },
-  computed: {
-    id() {
-      return this.$route.query.id;
-    },
-    name() {
-      return this.$store.getters["analyzers/getName"](this.id);
-    },
-    result() {
-      let result = this.$store.getters["analyzers/getResult"](this.id);
-
-      console.log("store state =");
-      console.log(this.$store.state);
-
-      console.log("result = ");
-      console.log(result);
-      return result;
-    }
-  },
-  data() {
-    return {
-      options: {
-        responsive: true,
-        maintainAspectRatio: false
-      },
-      charts_key: ''
-    };
-  }
+  components: { ResultChartStack }
 };
 </script>
 
@@ -97,15 +21,15 @@ export default {
 @import "../../assets/scss/_core-variables";
 @import "node_modules/bootstrap/scss/functions";
 
-.result-container {
-  width: calc(100vw - #{$sidebar-width} -16px);
+.fullpage-result-container {
+  width: calc(100vw - #{$sidebar-width} - 16px);
   height: calc(100vh - 10px);
   display: flex;
   flex-direction: column;
   overflow: hidden;
   padding-top: 8px;
 }
-.result {
+.fullpage-result {
   position: relative;
   flex-shrink: 1;
   flex-basis: 100vh;
@@ -116,7 +40,7 @@ export default {
   min-width: calc(100vw - #{$sidebar-width});
   max-width: calc(100vw - #{$sidebar-width});
 }
-.result canvas {
+.fullpage-result canvas {
   /*flex-grow: 1;*/
   width: calc(100vw - #{$sidebar-width});
 }
