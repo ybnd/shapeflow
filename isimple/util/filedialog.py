ea0177ac6a3d4d51837657c1bc376ede4e3e0c9c
ybnd <ybnd@tuta.io>
2020-07-07 13:45:23 +0200

    Fix threading issues on Windows

--- isimple/util/filedialog.py
+++ isimple/util/filedialog.py
@@ -2,7 +2,6 @@ from typing import List, Optional
 
 import os
 import subprocess
-import tkinter.filedialog
 
 
 __VIDEO_PATTERNS__ = ["*.mp4", "*.avi", "*.mov", "*.mpv", "*.mkv"]
@@ -19,7 +18,7 @@ def has_zenity():
         return False
 
 
-def load_file_dialog(title: str = None, patterns: List[str] = None, patterns_str: str = None) -> Optional[str]:
+def load_file_dialog(title: str = None, patterns: List[str] = None, patterns_str: str = '') -> Optional[str]:
     if title is None:
         title = 'Load...'
 
@@ -32,7 +31,7 @@ def load_file_dialog(title: str = None, patterns: List[str] = None, patterns_str
                 p = subprocess.Popen(
                     [
                         'zenity', '--file-selection',
-                        f'--file-filter', ' '.join(patterns),
+                        '--file-filter', ' '.join(patterns),
                     ], stdout=subprocess.PIPE, stderr=subprocess.PIPE
                 )
             else:
@@ -50,15 +49,30 @@ def load_file_dialog(title: str = None, patterns: List[str] = None, patterns_str
             return None
 
     else:
-        if len(patterns) > 0:
-            return tkinter.filedialog.askopenfilename(
-                title=title,
-                filetypes=[(patterns_str, ' '.join(patterns))]
-            )
-        else:
-            return tkinter.filedialog.askopenfilename(
-                title=title,
-            )
+        try:
+            if len(patterns) > 0:
+                p = subprocess.Popen(
+                    [
+                        'python', 'isimple/util/tk_filedialog.py',
+                        '--load', '--title', title, 
+                        '--filetypes', ' '.join(patterns),
+                        '--filedesc', patterns_str,
+                    ], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+            else:
+                p = subprocess.Popen(
+                    [
+                        'python', 'isimple/util/tk_filedialog.py',
+                        '--load', '--title', title, 
+                    ], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+            out, err = p.communicate()
+            if out:
+                return out.rstrip().decode('utf-8')
+            else:
+                return None
+        except subprocess.CalledProcessError:
+            return None
 
 
 def save_file_dialog(title: str = None, patterns: List[str] = None, patterns_str: str = None) -> Optional[str]:
@@ -92,15 +106,30 @@ def save_file_dialog(title: str = None, patterns: List[str] = None, patterns_str
             return None
 
     else:
-        if len(patterns) > 0:
-            return tkinter.filedialog.asksaveasfilename(
-                title=title,
-                filetypes=[(patterns_str, ' '.join(patterns))]
-            )
-        else:
-            return tkinter.filedialog.asksaveasfilename(
-                title=title,
-            )
+        try:
+            if len(patterns) > 0:
+                p = subprocess.Popen(
+                    [
+                        'python', 'isimple/util/tk_filedialog.py',
+                        '--save', '--title', title, 
+                        '--filetypes', ' '.join(patterns),
+                        '--filedesc', patterns_str,
+                    ], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+            else:
+                p = subprocess.Popen(
+                    [
+                        'python', 'isimple/util/tk_filedialog.py',
+                        '--save', '--title', title, 
+                    ], stdout=subprocess.PIPE, stderr=subprocess.PIPE
+                )
+            out, err = p.communicate()
+            if out:
+                return out.rstrip().decode('utf-8')
+            else:
+                return None
+        except subprocess.CalledProcessError:
+            return None
 
 
 def select_video() -> Optional[str]:
---
+++ isimple/util/tk_filedialog.py
@@ -0,0 +1,46 @@
+import argparse
+import tkinter as tk
+import tkinter.filedialog
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--save', dest='save', action='store_true')
+parser.add_argument('--load', dest='load', action='store_true')
+parser.add_argument('--title', type=str, default='Select a file')
+parser.add_argument('--filedesc', type=str, default=None)
+parser.add_argument('--filetypes', type=str, default=None)
+parser.set_defaults(save=None, load=None)
+
+root = tk.Tk()
+root.withdraw()
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    if args.save is None and args.load is None:
+        raise ValueError("Either '--save' or '--load' must be provided!")
+
+    if args.filetypes is not None:
+        if args.filedesc is None:
+            args.filedesc = args.filetypes
+        filetypes = [(args.filedesc, args.filetypes)]
+
+        if args.save:
+            path = tkinter.filedialog.asksaveasfilename(
+                title=args.title, filetypes=filetypes
+            )
+        elif args.load:
+            path = tkinter.filedialog.askopenfilename(
+                title=args.title, filetypes=filetypes
+            )
+    else:
+        if args.save:
+            path = tkinter.filedialog.asksaveasfilename(
+                title=args.title
+            )
+        elif args.load:
+            path = tkinter.filedialog.askopenfilename(
+                title=args.title
+            )
+
+    print(path)
