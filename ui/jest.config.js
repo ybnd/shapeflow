7cc4651160c60914dd5b36bbc9ac828339daf6b1
ybnd <ybnd@tuta.io>
2020-10-09 11:27:39 +0200

    Comment out *.vue and specify jsdom + options

--- ui/jest.config.js
+++ ui/jest.config.js
@@ -2,7 +2,11 @@ global.XMLHttpRequest = undefined;
 
 module.exports = {
   // tell Jest to handle `*.vue` files
-  moduleFileExtensions: ["js", "json", "vue"],
+  moduleFileExtensions: [
+    "js",
+    "json",
+    // "vue"
+  ],
   watchman: false,
   moduleNameMapper: {
     "^~/(.*)$": "<rootDir>/$1",
@@ -13,16 +17,20 @@ module.exports = {
     // process js with `babel-jest`
     "^.+\\.js$": "<rootDir>/node_modules/babel-jest",
     // process `*.vue` files with `vue-jest`
-    ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
+    // ".*\\.(vue)$": "<rootDir>/node_modules/vue-jest"
   },
   snapshotSerializers: ["<rootDir>/node_modules/jest-serializer-vue"],
   collectCoverage: true,
   collectCoverageFrom: [
-    "<rootDir>/components/**/*.vue",
-    "<rootDir>/pages/*.vue",
+    // "<rootDir>/components/**/*.vue",
+    // "<rootDir>/layouts/*.vue"
+    // "<rootDir>/pages/*.vue",
     "<rootDir>/static/*.js",
     "<rootDir>/store/*.js"
   ],
-  // testEnvironment: "node",
+  testEnvironment: "jsdom",
+  testEnvironmentOptions: {
+    url: "http://localhost:7951"
+  },
   testURL: "http://localhost:7951",
 };
