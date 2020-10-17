a51ca1973d4ea34e97c191ad03b99c52e43ddf36
ybnd <ybnd@tuta.io>
2020-10-17 22:09:32 +0200

    Pass on Python command in from_venv.py
    * e.g. if called with python3.7 try calling with python3.7 from the virtual environment
    * also clean up a bit

--- shapeflow/util/from_venv.py
+++ shapeflow/util/from_venv.py
@@ -8,27 +8,37 @@ import subprocess
 
 environment = '.venv'
 
-if __name__ == '__main__':
+
+def from_venv():
     script = sys.argv[1]        # the script to execute
     arguments = sys.argv[2:]    # and pass the rest of the arguments on to.
 
     if os.path.isdir(environment):
-        unix_dir = os.path.join(environment, 'bin')
-        win_dir = os.path.join(environment, 'Scripts')
-        
-        if os.path.isdir(unix_dir):
+        python = os.path.basename(sys.executable)
+
+        formats = {
+        # platform -> directory where the executable should be
+            'unix': os.path.join(environment, 'bin'),
+            'windows': os.path.join(environment, 'Scripts')
+        }
+        executable, platform, d = _get_executable(formats, python)
+
+        if platform == 'unix':
             pre = []
-            executable = os.path.join(unix_dir, 'python')
             shell = False
-        elif os.path.isdir(win_dir):
-            pre = ["set", f"PATH='%PATH%{os.path.abspath(win_dir)};\\'", "&&"]
-            executable = os.path.join(win_dir, 'python')
+        elif platform == 'windows':
+            pre = ["set", f"PATH='%PATH%{os.path.abspath(d)};\\'", "&&"]
             shell = True
         else:
-            raise OSError('The virtual environment has an unexpected format.')
+            raise EnvironmentError(
+                'The virtual environment has an unexpected format.'
+            )
 
         try:
-            subprocess.check_call(pre + [executable, script] + arguments, shell=shell)
+            subprocess.check_call(
+                pre + [executable, script] + arguments,
+                shell=shell
+            )
         except KeyboardInterrupt:
             # don't print exception on Ctrl+C
             pass
@@ -40,3 +50,20 @@ if __name__ == '__main__':
 
     else:
         raise EnvironmentError(f"No virtual environment in {environment}.")
+
+
+def _get_executable(formats, preferred_python):
+    for platform, d in formats.items():
+        if os.path.isdir(d) and len(os.listdir(d)) > 0:
+            preferred = os.path.join(d, preferred_python)
+            fallback = os.path.join(d, 'python')
+
+            if os.path.isfile(preferred):
+                return preferred, platform, d
+            elif os.path.isfile(fallback):
+                return fallback, platform, d
+    return 'python', ''
+
+
+if __name__ == '__main__':
+    from_venv()
