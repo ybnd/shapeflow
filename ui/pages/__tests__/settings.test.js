4a9b84100a102419acba230fda3f13f52f93360b
ybnd <ybnd@tuta.io>
2020-10-17 16:01:28 +0200

    Improve settings.test.vue
    * Mock schema/settings & don't render form

--- ui/pages/__tests__/settings.test.js
+++ ui/pages/__tests__/settings.test.js
@@ -1,6 +1,6 @@
 import {createLocalVue, mount, shallowMount} from "@vue/test-utils";
 import {afterAll, afterEach, beforeAll, beforeEach, describe, test} from "@jest/globals";
-import {cloneDeep} from 'lodash';
+import axios from 'axios';
 import {
   BButton, BCard, BFormCheckbox,
   BFormGroup,
@@ -16,19 +16,14 @@ import PageHeader from "../../components/header/PageHeader";
 import PageHeaderItem from "../../components/header/PageHeaderItem";
 
 import settings from '../settings';
-import {killServer, startServer} from "../../static/shapeflow";
 import {retryOnce} from "../../static/util";
-import {waitSync} from "../../static/shapeflow";
 import {get_schemas, get_settings, clear_cache, clear_db, get_cache_size, open_root} from "../../static/api";
 import Vuex from "vuex";
-import SchemaForm from "../../components/config/SchemaForm";
-import SchemaCategory from "../../components/config/SchemaCategory";
-import SchemaDefinition from "../../components/config/SchemaDefinition";
-import SchemaField from "../../components/config/SchemaField";
+import flushPromises from "flush-promises";
 
 
-var SCHEMAS;
-var SETTINGS;
+const SCHEMA = { dummy: 'schema' };
+var SETTINGS = { dummy: 'SETTINGS'};
 
 var localVue;
 var store;
@@ -36,17 +31,6 @@ var store;
 const analyzers_newnotice = jest.fn();
 const settings_set = jest.fn();
 const settings_sync = jest.fn();
-
-
-beforeAll(async () => {
-  startServer();
-  SCHEMAS = await retryOnce(get_schemas);  // todo: use sf.py dump & mock axios instead
-  SETTINGS = await retryOnce(get_settings);
-});
-
-afterAll(() => {
-  killServer();
-});
 
 beforeEach(() => {
   localVue = createLocalVue();
@@ -64,7 +48,7 @@ beforeEach(() => {
         namespaced: true,
         getters: {
           getSettingsSchema() {
-            return SCHEMAS.settings;
+            return SCHEMA;
           }
         }
       },
@@ -72,17 +56,27 @@ beforeEach(() => {
         namespaced: true,
         getters: {
           getSettingsCopy() {
-            return cloneDeep(SETTINGS);
+            return SETTINGS;
           }
         },
         actions: {
           set: settings_set,
-          sync: settings_set,
+          sync: settings_sync,
         }
       },
     },
   });
 });
+
+jest.mock('axios');
+
+axios.get.mockResolvedValue({
+  status: 200, data: 1000,  // mock db/cache size responses
+})
+
+afterEach(() => {
+  jest.clearAllMocks();
+})
 
 
 function factory() {
@@ -91,19 +85,9 @@ function factory() {
     stubs: {
       PageHeader,
       PageHeaderItem,
-      SchemaForm,
-      SchemaCategory,
-      SchemaDefinition,
-      SchemaField,
+      SchemaForm: { template: '<div class="form-stub" />' },
       BButton,
       BCard,
-      BRow,
-      BFormRow,
-      BFormGroup,
-      BInputGroup,
-      BFormSelect,
-      BFormInput,
-      BFormCheckbox,
     }
   })
 }
@@ -121,7 +105,63 @@ test('mount', async () => {
   expect(w.find('.settings-clear-cache').exists()).toBeTruthy()
 
   // there is a form
-  expect(w.findAllComponents(SchemaForm).wrappers.length).toBeGreaterThan(1);
-  expect(w.findAllComponents(SchemaCategory).wrappers.length).toBeGreaterThan(1);
-  expect(w.findAllComponents(SchemaField).wrappers.length).toBeGreaterThan(1);
+  expect(w.find('.form-stub').exists()).toBeTruthy();
+
+  expect(settings_sync).toHaveBeenCalled();
+  // expect(axios.get).toHaveBeenCalledWith('/api/db/disk-size', expect.anything(), expect.anything());
+  expect(axios.get).toHaveBeenCalledWith('/api/cache/disk-size', expect.anything());
 });
+
+test('set settings', async () => {
+  const w = factory();
+
+  expect(settings_set).not.toHaveBeenCalled();
+  await w.find('.settings-save').trigger('click');
+  expect(settings_set).toHaveBeenCalledWith(expect.anything(), { settings: SETTINGS });
+});
+
+test('clear db', async () => {
+  const w = factory();
+
+  expect(axios.post).not.toHaveBeenCalled();
+  axios.post.mockResolvedValueOnce({
+    status: 200, data: true,
+  })
+  await w.find('.settings-clear-db').trigger('click');
+  expect(axios.post).toHaveBeenCalledWith('/api/db/forget', expect.anything(), expect.anything());
+});
+
+test('clear cache', async () => {
+  const w = factory();
+
+  expect(axios.post).not.toHaveBeenCalled();
+  axios.post.mockResolvedValueOnce({
+    status: 200, data: true,
+  })
+  await w.find('.settings-clear-cache').trigger('click');
+  expect(axios.post).toHaveBeenCalledWith('/api/cache/clear', expect.anything(), expect.anything());
+});
+
+test('open root', async () => {
+  const w = factory();
+
+  expect(axios.post).not.toHaveBeenCalled();
+
+  // open root
+  axios.post.mockResolvedValue({
+    status: 200, data: true,
+  })
+  await w.find('.settings-open-root').trigger('click');
+  expect(axios.post).toHaveBeenCalledWith('/api/open_root', expect.anything(), expect.anything());
+  expect(analyzers_newnotice).not.toHaveBeenCalled();
+
+  // can't open root
+  axios.post.mockResolvedValueOnce({
+    status: 404, data: null,
+  });
+  await w.find('.settings-open-root').trigger('click');
+  await flushPromises();
+  expect(axios.post).toHaveBeenCalledWith('/api/open_root', expect.anything(), expect.anything());
+  expect(analyzers_newnotice).toHaveBeenCalled();
+});
+
