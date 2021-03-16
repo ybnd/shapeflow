092f018ce632b8f0d70a8509617bed00672aaa53
ybnd <ybnd@tuta.io>
2021-03-16 20:35:05 +0100

    Update post-deploy script

--- shapeflow/setup/post-deploy.py
+++ shapeflow/setup/post-deploy.py
@@ -1,14 +1,19 @@
 from sys import executable
-from subprocess import check_call
+from subprocess import check_call, CalledProcessError
 
 
 def sf(*args):
     check_call([executable, "sf.py", *args])
 
-print("\nRunning setup/post-deploy.py ...\n")
 
-sf("setup-cairo", "--cleanup")
-sf("get-compiled-ui", "--replace")
-sf("declutter")
+try:
+    print("\nRunning setup/post-deploy.py ...\n")
 
-print("\n\nAll done.")
+    sf("setup-cairo", "--cleanup")
+    sf("get-compiled-ui", "--replace")
+    sf("declutter")
+
+    print("\n\nDone.")
+except CalledProcessError as e:
+    print(f"Command failed: {e.cmd}")
+    exit(1)
